13d405abda5912c59e0297e8b77bd6fdd6ce2e74
Rutuja kurhekar <110963226+12rutuja@users.noreply.github.com>
2025-05-20 12:34:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Rutuja,welcome to the node js application\n');
+  res.end('Hello Rutuja\n');
   
 });
 
