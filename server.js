1d48337793a508c639a7a8abe9fa4b3dbcaad0a3
Rutuja kurhekar <110963226+12rutuja@users.noreply.github.com>
2025-05-22 11:49:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Rutuja,congratilations for getting job in impetus\n');
+  res.end('Hello Rutuja\n');
   
 });
 
